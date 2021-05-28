b5a95c52672646017fb9a67eb7871b575f3c6752
AneezaSabirButt <aneezasabirbutt123@gmail.com>
2021-05-28 20:51:38 +0500

    question1 attempted

---
+++ question1.js
@@ -0,0 +1,11 @@
+// divisibilty of 3
+function is_divisibile_by_3(num) {
+    if (num % 3 === 0) {
+        console.log(true);
+    }else {
+        console.log(false);
+    }
+}
+is_divisibile_by_3(93)
+is_divisibile_by_3(5673)
+is_divisibile_by_3(545)
